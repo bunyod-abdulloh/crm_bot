b66e9b5948ba51f3b6c6e44f11de07266b914bee
bunyod-abdulloh <90672691+Bunyod-Abdulloh@users.noreply.github.com>
2024-07-03 23:06:21 +0500

    Updated

--- bot/utils/set_bot_commands.py
+++ bot/utils/set_bot_commands.py
@@ -6,6 +6,7 @@ from aiogram.types import BotCommandScopeAllPrivateChats
 async def set_default_commands(bot: Bot):
     commands = [
         BotCommand(command="/start", description="Botni ishga tushirish"),
-        BotCommand(command="/help", description="Yordam"),
+        BotCommand(command="/habar", description="Adminga habar yuborish"),
+        BotCommand(command="/admin", description="Admin bo'limi"),
     ]
     await bot.set_my_commands(commands=commands, scope=BotCommandScopeAllPrivateChats())
